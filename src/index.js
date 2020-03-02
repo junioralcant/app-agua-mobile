1e961726596d241fe7b0bb45ffdd7c3f5c3814f3
Junior Marques <junioralcant111@gmail.com>
2020-03-02 15:46:14 -0300

    Configurcao do onesiganl

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+import OneSignal from 'react-native-onesignal';
 
 import Routes from './routes';
 
-export default function App() {
-  return <Routes />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+    OneSignal.init('b518b1f3-d572-47b2-8dd3-457dc1eec757');
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {};
+
+  onOpened = notification => {};
+
+  onIds = id => {};
+
+  render() {
+    return <Routes />;
+  }
 }
+
+export default App;
